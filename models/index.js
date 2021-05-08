e608b1b4c82234cb0c47277b2a6178994043f2bf
chris <c.caressimo@gmail.com>
2021-05-08 12:56:25 -0400

    linked front end

--- models/index.js
+++ models/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-=======
 'use strict';
 const fs = require('fs');
 const path = require('path');
@@ -14,35 +5,11 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
->>>>>>> c63ab446a91417a46392f344852603ccd86ea3a6
 const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-<<<<<<< HEAD
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
-Object.keys(db).forEach((modelName) => {
-=======
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 fs
@@ -55,15 +22,10 @@ fs
     db[model.name] = model;
   });
 Object.keys(db).forEach(modelName => {
->>>>>>> c63ab446a91417a46392f344852603ccd86ea3a6
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-<<<<<<< HEAD
 module.exports = db;
-=======
-module.exports = db;
->>>>>>> c63ab446a91417a46392f344852603ccd86ea3a6
--- seeders/20210507010524-mixafy.js
+++ seeders/20210507010524-mixafy.js
@@ -2,15 +2,23 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('mixafy', [{
-      artist: 'Blink 182',
-      song: 'Feeling This',
-      shared_link: 'https://open.spotify.com/track/5yWcR25EXM7x0nN9CxD6p6?si=5ff2b7efec9d4c55',
-      embed_link: '<iframe src="https://open.spotify.com/embed/track/5yWcR25EXM7x0nN9CxD6p6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
-      genre_id: 'Pop-Punk',
-      createdat: new Date(),
-      updatedat: new Date()
-  }], {});
+    await queryInterface.bulkInsert(
+      "mixafy",
+      [
+        {
+          artist: "Ace of Base",
+          song: "The Sign",
+          shared_link:
+            "https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE?si=39927f4240ea46ca",
+          embed_link:
+            "https://open.spotify.com/embed/track/0hrBpAOgrt8RXigk83LLNE",
+          genre_id: "90's Pop",
+          createdat: new Date(),
+          updatedat: new Date(),
+        },
+      ],
+      {}
+    );
 },
 
   down: async (queryInterface, Sequelize) => {
